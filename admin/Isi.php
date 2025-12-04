c031dbdafc220c474695a448d3a51be8a55d7adb
Indah0806 <indahyanti443@gmail.com>
2025-12-04 22:49:21 +0700

    update file

--- admin/Isi.php
+++ admin/Isi.php
@@ -131,7 +131,7 @@
 <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      <form method="POST" action="Edit.php">
+      <form method="POST" action="Isi.php">
         <div class="modal-header">
           <h5 class="modal-title" id="editModalLabel">Edit Jadwal Rapat</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
